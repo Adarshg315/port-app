e82015bd6873a3399c79b84483afb4636b0fe18f
AdarshGyt <adarshgoyal@gytworkz.com>
2022-08-30 23:23:20 +0530

    updated website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
+import { Route, Switch } from "react-router-dom"
+import Landing from "./components/Landing"
 
 const App = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route component={Error} />
-    </Switch>
-  );
-};
+	return (
+		<Switch>
+			<Route exact path="/" component={Landing} />
+			<Route component={Error} />
+		</Switch>
+	)
+}
 
-export default App;
+export default App
---
+++ src/components/Landing.jsx
@@ -0,0 +1,32 @@
+import NoPhone from "../../src/assests/icons/software-developer-work-on-computer-programmer-coder-svgrepo-com.svg"
+const Landing = () => {
+	return (
+		<div
+			style={{
+				display: "grid",
+				flexDirection: "row",
+				alignContent: "center",
+				justifyContent: "center",
+				textAlign: "center",
+			}}
+		>
+			<h1>Adarsh Goyal</h1>
+			<h2>Software Developer</h2>
+
+			<h2>
+				<a
+					href="https://linktr.ee/adarshgoyal"
+					target="_blank"
+					rel="noreferrer"
+				>
+					https://linktr.ee/adarshgoyal
+				</a>
+			</h2>
+			<p>
+				<img src={NoPhone} alt="no cell phone svg" />
+			</p>
+		</div>
+	)
+}
+
+export default Landing
